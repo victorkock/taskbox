ac15efa172dfea83507842809634f6de1caadeae
victorkock <victorkock@gmail.com>
2023-05-25 11:01:59 +0200

    Added InboxScreen component and story. Added mockServiceWorker to mock API calls

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,13 @@
 import '../src/index.css';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
 
 //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [mswDecorator],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
